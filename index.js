5b1171acd1ea5472e32f4b3affec172dfe0c8b8a
Dowen Robinson <35748031+enahsor@users.noreply.github.com>
2022-05-03 01:47:25 -0500

    Implemented an exclusion list to remove non service dirs

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const fs = require("fs");
+const { join } = require("path");
+
+const EXCLUDE = [".github/"];
+
+const isFolder = (path) => {
+  return path.includes("/");
+};
+
+const getBase = (path) => {
+  if (isFolder(path)) {
+    return path.split("/")[0];
+  }
+  return false;
+};
 
 const main = async () => {
+  const root = core.getInput("services-root");
   const token = core.getInput("token");
   if (!token) {
     throw new Error(`No token provided`);
@@ -13,22 +27,26 @@ const main = async () => {
   const { name: owner } = repository.owner;
   const { name: repo } = repository;
   const refs = payload.commits.map((commit) => commit.id);
-
   const values = refs.map(async (ref) => {
     const commit = await octokit.request(
       `GET /repos/${owner}/${repo}/commits/${ref}`
     );
     return commit.data.files;
   });
+  let files = (await Promise.all(values)).flat();
 
-  const commits = await Promise.all(values);
-  console.log(commits);
+  // Get all directories not excluded
+  files = files.filter(({ filename }) => {
+    return isFolder(filename) && !EXCLUDE.includes(getBase(filename));
+  });
 
-  const services = core.getInput("services");
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  const affectedServices = files.map(({ filename }) => {
+    return getBase(filename);
+  });
+
+  core.setOutput("affected-services", affectedServices);
   console.log(`Services : ${services}`);
-  //console.log(`Files updated are: ${JSON.stringify(files)}`);
+  console.log(`Files: ${JSON.stringify(affectedServices)}`);
   console.log(`The event payload: ${JSON.stringify(payload, undefined, 2)}`);
 };
 
